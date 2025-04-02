9f9bbb6dfd0ea4b2023899933e8809677b03a4ad
jo1-8899-2x <jongashi010@gmail.com>
2025-04-02 19:12:08 +0200

    ushtrime

---
+++ Module 29/main.js
@@ -0,0 +1,54 @@
+var programmingLanguages=['JavaScript','PHP','Python'];
+
+console.log(programmingLanguages);
+
+console.log(programmingLanguages[0]);
+
+
+programmingLanguages.push('Java');
+
+console.log(programmingLanguages);
+
+programmingLanguages.pop('Java');
+
+console.log(programmingLanguages);
+
+programmingLanguages.shift();
+
+console.log(programmingLanguages);
+
+programmingLanguages.unshift('c++');
+
+console.log(programmingLanguages);
+
+
+programmingLanguages.splice(0,2,'HTML');
+console.log(programmingLanguages);
+
+console.log(Math.random()*5);
+
+console.log(Math.floor(Math.random())*5);
+
+
+
+var students =['John','Doe'];
+
+var [s1,s2]=students;
+
+console.log(s1)
+console.log(s2)
+
+
+var places =["London","Paris","New York","Berlin"];
+
+var [firstPlace, ,secondPlace] = places;
+
+console.log(secondPlace)
+
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+
+var[first,second, ...otherNumbers]= numbers;
+
+console.log(first);
+console.log(second);
+console.log(otherNumbers.toString());
