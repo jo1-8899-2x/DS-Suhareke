223c1d3b9ac05e8e9da8a7985855895a028a671a
jo1-8899-2x <jongashi010@gmail.com>
2025-04-23 19:18:22 +0200

    module 31

---
+++ Module 31/main.js
@@ -0,0 +1,34 @@
+ var i=0
+ do{
+    i = i+1;
+    console.log(i);
+ }while(i<5);
+
+ console.log("--------------------------------")
+
+ var j=0
+ while(j<5){
+    j=j+1;
+    console.log(j);
+ }
+ console.log("--------------------------------")
+
+
+ let n=0;
+ let x=0;
+
+ while(n<3){
+n++;
+x+=n;
+console.log(n)
+console.log(x)
+
+ }
+
+ console.log("--------------------------------");
+
+ for(var i=0; i<5; i++){
+    console.log("Value of i is: "+i)
+ }
+ console.log("--------------------------------");
+
