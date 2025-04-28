f85c2ddc7d089cb4c47b81815965f5dfa11844d7
jo1-8899-2x <jongashi010@gmail.com>
2025-04-28 19:16:49 +0200

    medium

---
+++ Module 31/medium.js
@@ -0,0 +1,11 @@
+function checkPalindromes(words)
+   for(var i=0; i<words.length; i++){
+    if(words[i] === words[i].split('').reverse().join('')){
+        console.log("YES");
+    }else
+    {
+        console.log("NO");
+    }
+   
+}
+checkPalindromes(["kimik","level","module"]);
