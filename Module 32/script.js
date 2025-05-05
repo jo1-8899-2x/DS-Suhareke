e8492c61cace6a93f96b74f0fdffeeb77897ef63
jo1-8899-2x <jongashi010@gmail.com>
2025-05-05 18:51:50 +0200

    module 32

--- Module 32/script.js
+++ Module 32/script.js
@@ -30,6 +30,42 @@ document.getElementById("result5").innerHTML=regex.match(regex);*/
 var regex=/i/g;
 document.getElementById("result6").innerHTML=text.match(regex);*/
 
-var text="The best school in the world is Digital School";
+/*var text="The best school in the world is Digital School";
 var regex=/[abc]/g;
-document.getElementById("result7").innerHTML=text.match(regex);
+document.getElementById("result7").innerHTML=text.match(regex);*/
+
+/*var text= "My school is the best school in the world";
+var regex=/(top|best|school)/g;
+document.getElementById("result9").innerHTML=text.match(regex);*/
+
+/*var text="100 percent!";
+var regex=/\d/g;
+document.getElementById("result10").innerHTML=text.match(regex);*/
+
+/*var text="My school is the best school in the world!";
+var regex=/\s/g;
+document.getElementById("result11").innerHTML=text.match(regex);*/
+
+/*var text="heeey, how are you";
+var regex=/\e+/g;
+document.getElementById("result12").innerHTML=text.match(regex);*/
+
+//var text="so, i hope we will see eachother soon again...";
+//var regex=/so*/g;
+//document.getElementById("result13").innerHTML=text.match(regex);
+
+/*var text="hey,hi,hiii";
+var regex=/hi?/g;
+document.getElementById("result14").innerHTML=text.match(regex);*/
+
+/*var text="hello, hellooo, hellooo";
+var regex=/o{3,5}/g;
+document.getElementById("result15").innerHTML=text.match(regex);*/
+
+/*var text="bestfriend, boyfriend, girlfriend";
+var regex=/end$/g;
+document.getElementById("result17").innerHTML=text.match(regex);*/
+
+var text="cat, catalog, category";
+var regex=/^cat/g;
+document.getElementById("result18").innerHTML=text.match(regex);
